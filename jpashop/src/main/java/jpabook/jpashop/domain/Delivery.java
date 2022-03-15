4cf0e8111f3b0cc030a6d25c137b5645c93af055
kdh30806 <kdh30806@naver.com>
2022-03-15 23:59:59 +0900

    실전예제 6 값 매핑

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -13,11 +13,67 @@ public class Delivery {
 	@Column(name = "DELIVERY_ID")
 	private Long id;
 	
-	private String city;
-	private String street;
-	private String zipcode;
+	private Address address;
 	private DeliveryStatus status;
 	
 	@OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
 	private Order order;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public DeliveryStatus getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(DeliveryStatus status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the order
+	 */
+	public Order getOrder() {
+		return order;
+	}
+
+	/**
+	 * @param order the order to set
+	 */
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	
 }
